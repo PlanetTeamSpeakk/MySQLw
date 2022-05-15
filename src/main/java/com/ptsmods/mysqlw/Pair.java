a196716939a673f0c663eb0f256201d206218115
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2022-05-15 14:48:39 +0200

    Add hashCode and equals methods to Pair

--- src/main/java/com/ptsmods/mysqlw/Pair.java
+++ src/main/java/com/ptsmods/mysqlw/Pair.java
@@ -1,8 +1,10 @@
 package com.ptsmods.mysqlw;
 
+import java.util.Objects;
+
 public class Pair<L, R> {
-    private L left;
-    private R right;
+    private final L left;
+    private final R right;
 
     public Pair(L left, R right) {
         this.left = left;
@@ -17,4 +19,16 @@ public class Pair<L, R> {
         return right;
     }
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Pair)) return false;
+		Pair<?, ?> pair = (Pair<?, ?>) o;
+		return Objects.equals(getLeft(), pair.getLeft()) && Objects.equals(getRight(), pair.getRight());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getLeft(), getRight());
+	}
 }
